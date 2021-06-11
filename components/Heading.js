a388ea94f514d3d3ee4cc4840cfd56031b47e5b5
Andrew Fernando <a.fernando@fernandonet.co.uk>
2021-06-11 14:18:49 +0100

    Move heading out to component

---
+++ components/Heading.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Heading() {
+  return (
+    <Head>
+      <title>RetroCharts</title>
+      <meta name="description" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
