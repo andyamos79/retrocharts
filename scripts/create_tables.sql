c393ae1ca43d98a7a0e11b89195a5800dd00a617
Andrew Fernando <a.fernando@fernandonet.co.uk>
2021-06-22 02:03:15 +0100

    Update SQLlite scripts

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,15 +1,21 @@
 
-CREATE TABLE userValues (
-  id INTEGER PRIMARY KEY,
-  user_id INTEGER NOT NULL,
-  date TEXT NOT NULL,
-  category TEXT NOT NULL,
-  value INTEGER
-);
+-- CREATE TABLE userValues (
+--   id INTEGER PRIMARY KEY,
+--   user_id INTEGER NOT NULL,
+--   date TEXT NOT NULL,
+--   category TEXT NOT NULL,
+--   value INTEGER,
+--   UNIQUE(user_id, date, category)
+-- );
 
-CREATE TABLE users (
-   user_id INTEGER PRIMARY KEY,
-   first_name TEXT NOT NULL,
-   last_name TEXT NOT NULL
-);
-  
+-- CREATE TABLE users (
+--    user_id INTEGER PRIMARY KEY,
+--    first_name TEXT NOT NULL,
+--    last_name TEXT NOT NULL,
+--    UNIQUE(first_name, last_name)
+-- );
+
+-- DROP TABLES
+
+-- DROP TABLE userValues;
+-- DROP TABLE users;
